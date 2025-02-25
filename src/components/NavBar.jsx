62db02936acedf1f43b2e1037dc5878ad2b5d3b7
Thomassaiag <thomas.saiag@gmail.com>
2025-02-25 18:27:09 +0100

    added style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Wrapper from '../assets/wrappers/navbar'
+import Wrapper from '../assets/wrappers/Navbar'
 
 export const NavBar = () => {
 	return (
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/NavBar";
+
 const HomeLayout = () => {
 	return (
 		<>
 			<NavBar />
-			<Outlet />
+			<section className="page">
+				<Outlet />
+			</section>
 		</>
 	);
 };
